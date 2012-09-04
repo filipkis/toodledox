70a48381b32be384670513a2bee5e871921a0509
Filip Kis <filip.kis@gmail.com>
2012-09-04 04:44:31 +0200

    add number tasks view

---
+++ ToodleDoX/TaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  TaskViewController.h
+//  ToodleDoX
+//
+//  Created by Filip Kis on 12/9/4.
+//  Copyright (c) 2012 Filip Kis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TaskViewController : NSObject
+
+@end
---
+++ ToodleDoX/TaskViewController.m
@@ -0,0 +1,13 @@
+//
+//  TaskViewController.m
+//  ToodleDoX
+//
+//  Created by Filip Kis on 12/9/4.
+//  Copyright (c) 2012 Filip Kis. All rights reserved.
+//
+
+#import "TaskViewController.h"
+
+@implementation TaskViewController
+
+@end
